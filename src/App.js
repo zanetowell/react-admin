d7c336c14db82aa121e4c4820b09ba6e711915fb
zanetowell <zanetowell@gmail.com>
2023-07-05 16:04:35 -0400

    Adds line and pie charts and pages

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Form from "./scenes/form";
 import Calender from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
 // import Geography from "./scenes/geography";
 
 
@@ -36,8 +36,8 @@ function App() {
                 <Route path="/calendar" element={<Calender />} />
                 <Route path="/faq" element={<FAQ />} />
                 <Route path="/bar" element={<Bar />} />
-                {/* <Route path="/pie" element={<Pie />} /> */}
-                {/* <Route path="/line" element={<Line />} /> */}
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
                 {/* <Route path="/geography" element={<Geography />} /> */}
               </Routes>
             </main>
