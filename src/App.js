67b2d298115906b83bc56851ca230e58c77016d4
zanetowell <zanetowell@gmail.com>
2023-07-05 15:19:38 -0400

    Adds calendar page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calender from "./scenes/calendar";
+// import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calender from "./scenes/calendar";
 
 
 function App() {
@@ -33,12 +33,12 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form/>} />
+                <Route path="/calendar" element={<Calender />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
                 {/* <Route path="/faq" element={<FAQ />} /> */}
                 {/* <Route path="/geography" element={<Geography />} /> */}
-                {/* <Route path="/calendar" element={<Calender />} /> */}
               </Routes>
             </main>
           </div>
